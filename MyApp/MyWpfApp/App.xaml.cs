01e72a8a6fa8745b5655b7b3461ec03d3bfe81df
ZeinVan <124338583+ZeinVan@users.noreply.github.com>
2023-03-17 11:00:53 +0800

    Add files via upload

---
+++ MyApp/MyWpfApp/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyWpfApp
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public static void ExecConsoleApp(string arg) { 
+            //获取ExampleWpfApp.exe文件的完整路径
+            string path=System.Reflection.Assembly .GetExecutingAssembly().Location;
+            //得到ExampleConsoleApp.exe的完整路径
+            path = path.Replace(@"MyWpfApp\bin\Debug\MyWpfApp.exe", @"MyConsoleApp\bin\Debug\MyConsoleApp.exe");
+            System.Diagnostics.Process.Start(path,arg);
+        }
+    }
+}
